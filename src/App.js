c15f20f47d85f0c120a05ce60f63676a667337f6
Rene <tak_esouza@uolinc.com>
2018-06-01 16:25:30 -0300

    Atualizando os states dos componentes

--- src/App.js
+++ src/App.js
@@ -3,13 +3,53 @@ import './App.css';
 import Person from './Person/Person';
 
 class App extends Component {
+
+  state = {
+    persons:[
+      {
+        name: "Rene",
+        age: 33
+      },
+      {
+        name: "Érica",
+        age: 29
+      },
+      {
+        name: "Helena",
+        age: 0
+      }
+    ]
+  }
+
+  switchNameHandler = () =>{
+    //console.log('Was clicked');
+    // DON'T do This -> this.state.persons[0].name = "Rene Souza";
+    this.setState({
+      persons:[
+        {
+          name: "Rene Souza",
+          age: 33
+        },
+        {
+          name: "Érica",
+          age: 29
+        },
+        {
+          name: "Helena",
+          age: 1
+        }
+      ]
+    })
+  }
+
   render() {
     return (
       <div className="App">
         <h1>React App</h1>
-        <Person name="Rene" age="33"/>
-        <Person name="Érica" age="29">My hobbies: read</Person>
-        <Person name="Helena" age="01"/>
+        <button onClick={this.switchNameHandler}>Switch name</button>
+        <Person name={this.state.persons[0].name} age={this.state.persons[0].age}/>
+        <Person name={this.state.persons[1].name} age={this.state.persons[1].age}>My hobbies: read</Person>
+        <Person name={this.state.persons[2].name} age={this.state.persons[2].age}/>
       </div>
     );
   }
