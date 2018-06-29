db337b163d4bd128d2da2db436adc6f763d2f7b1
Rene <tak_esouza@uolinc.com>
2018-06-29 13:42:26 -0300

    [React-course] - Usando Hoc para retornar statefull components

--- src/components/hoc/withClass.js
+++ src/components/hoc/withClass.js
@@ -1,11 +1,21 @@
-import React from 'react';
+import React, { Component } from 'react';
 
+// const withClass = (WrappedComponent, className) =>{
+//     return (props) => ( 
+//         <div className = {className}>
+//             <WrappedComponent {...props} />
+//         </div>
+//     )
+// }
 const withClass = (WrappedComponent, className) =>{
-    return (props) => ( 
-        <div className = {className}>
-            <WrappedComponent {...props} />
-        </div>
-    )
+    return class extends Component{ //class on Demand
+        render(){
+            return( 
+                <div className = {className}>
+                    <WrappedComponent {...this.props} />
+                </div>
+            );
+        }
+    }
 }
-
 export default withClass;
