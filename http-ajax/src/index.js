89db6dbce68dcbaae69f2ac2a32bca6a3ffe0d5c
Rene <tak_esouza@uolinc.com>
2018-07-10 12:11:21 -0300

    [react course] - Criando e removendo interceptors

--- http-ajax/src/index.js
+++ http-ajax/src/index.js
@@ -2,7 +2,35 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
+import axios from 'axios';
 import registerServiceWorker from './registerServiceWorker';
+
+//criando interceptors
+
+var reqInterceptor = axios.interceptors.request.use(
+    request => {
+        console.log(request);
+        return request;
+    },
+
+    error => {
+        return Promise.reject(error)
+    }
+);
+
+var resInterceptor = axios.interceptors.response.use(
+    response =>{
+        console.log(response);
+        return response;
+    },
+    error => {
+        console.log(error);
+    }
+);
+
+//romovendo interceptors
+// axios.interceptors.request.eject(reqInterceptor);
+// axios.interceptors.response.eject(resInterceptor);
 
 ReactDOM.render( <App />, document.getElementById( 'root' ) );
 registerServiceWorker();
