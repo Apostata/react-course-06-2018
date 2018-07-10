02afef1ef0731758d3f3366d77b4ab39ace363b3
Rene <tak_esouza@uolinc.com>
2018-07-10 13:16:54 -0300

    [react course] - Usando instancias de axios para alterar os defaults

--- http-ajax/src/index.js
+++ http-ajax/src/index.js
@@ -3,6 +3,7 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import axios from 'axios';
+
 import registerServiceWorker from './registerServiceWorker';
 
 //criando interceptors
