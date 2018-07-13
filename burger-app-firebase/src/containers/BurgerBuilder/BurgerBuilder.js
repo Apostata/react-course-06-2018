a3388b68ebc0ed287c8b0a429c890218cd8d0d6c
Rene <tak_esouza@uolinc.com>
2018-07-13 10:46:41 -0300

    [react-course] - Criando um HOC para tratamento de erros usando os interceptors do axios (Bem interessante)

--- burger-app-firebase/src/containers/BurgerBuilder/BurgerBuilder.js
+++ burger-app-firebase/src/containers/BurgerBuilder/BurgerBuilder.js
@@ -4,6 +4,8 @@ import BuildControls from '../../components/Burger/BuildControls/BuildControls';
 import Modal from '../../components/UI/Modal/Modal';
 import OrderSummary from '../../components/Burger/OrderSummary/OrderSummary';
 import Spinner from '../../components/UI/Spinner/Spinner';
+import withErrorHandler from '../../hoc/withErrorHandler/withErrorHandler';
+
 import INGREDIENT_PRICES from '../../json/ingredient_prices.json';
 import axios from '../../axios-orders';
 
@@ -154,4 +156,4 @@ class BurgerBuilder extends Component{
     }
 }
 
-export default BurgerBuilder;
+export default withErrorHandler(BurgerBuilder, axios);
---
+++ burger-app-firebase/src/hoc/withErrorHandler/withErrorHandler.js
@@ -0,0 +1,44 @@
+import React, {Component, Fragment} from 'react';
+import Modal from '../../components/UI/Modal/Modal';
+
+const withErrorHandler = (WrappedComponent, axios ) =>{
+    return class extends Component{ //classe anonima
+        state = {
+            error: null
+        }
+
+        componentDidMount(){
+            axios.interceptors.response.use(req =>{ // na requisição limpa o modal
+                this.setState({
+                    error: null
+                });
+                return req;
+            });
+
+            axios.interceptors.response.use(res => res, error =>{
+               this.setState({
+                   error:error
+               });
+            })
+        }
+
+        errorConfirmedHandler(){
+            this.setState({
+                error:null
+            })
+        }        
+
+        render(){
+            return (
+                <Fragment>
+                    <Modal show={this.state.error} backdropClick={this.errorConfirmedHandler.bind(this)}>
+                       {this.state.error ? this.state.error.message : null}
+                    </Modal>
+                    <WrappedComponent {...this.props} />
+                </Fragment>
+            );
+        }
+    }
+};
+
+export default withErrorHandler; //exporta função
