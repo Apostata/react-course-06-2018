552a9f2486fa1e45fa85c12de02fea0aa00ad762
Rene <tak_esouza@uolinc.com>
2018-07-18 15:44:18 -0300

    [react-course] - Redirecionando com history.push ou history.replace

--- http-ajax-routing/src/containers/NewPost/NewPost.js
+++ http-ajax-routing/src/containers/NewPost/NewPost.js
@@ -7,8 +7,8 @@ class NewPost extends Component {
     state = {
         title: '',
         content: '',
-        author: 'Rene',
-        submited: false
+        author: 'Rene'
+        //submited: false
     }
 
     componentDidMount(){
@@ -25,31 +25,29 @@ class NewPost extends Component {
         axios.post('/posts', data)
         .then(response =>{
             console.log(response);
-            this.setState({
-                submited: true
-            });
+            //this.props.history.push('/posts/');
+            //ou
+            this.props.history.replace('/posts/');
+            
         });
     }
 
     render () {
-        let newPost = 
-        <section className="NewPost">
-            <h1>Add a Post</h1>
-            <label>Title</label>
-            <input type="text" value={this.state.title} onChange={(event) => this.setState({title: event.target.value})} />
-            <label>Content</label>
-            <textarea rows="4" value={this.state.content} onChange={(event) => this.setState({content: event.target.value})} />
-            <label>Author</label>
-            <select value={this.state.author} onChange={(event) => this.setState({author: event.target.value})}>
-                <option value="Rene">Rene</option>
-                <option value="Helena">Helena</option>
-            </select>
-            <button onClick={()=>this.postDataHandler()}>Add Post</button>
-        </section>;
-
-        if(this.state.submited) newPost = <Redirect to="/posts" />;
-
-        return newPost;            
+        return (
+            <section className="NewPost">
+                <h1>Add a Post</h1>
+                <label>Title</label>
+                <input type="text" value={this.state.title} onChange={(event) => this.setState({title: event.target.value})} />
+                <label>Content</label>
+                <textarea rows="4" value={this.state.content} onChange={(event) => this.setState({content: event.target.value})} />
+                <label>Author</label>
+                <select value={this.state.author} onChange={(event) => this.setState({author: event.target.value})}>
+                    <option value="Rene">Rene</option>
+                    <option value="Helena">Helena</option>
+                </select>
+                <button onClick={()=>this.postDataHandler()}>Add Post</button>
+            </section>
+        );
     }
 }
 
