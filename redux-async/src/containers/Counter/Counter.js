872e710c47e48bb5dfd495aa57e7f0a8de217962
Rene <rene.souza@gmail.com>
2018-08-03 17:16:12 -0300

    [react-course] - Usando redux-thunk para trabalhar com ações assíncronas

--- redux-async/src/containers/Counter/Counter.js
+++ redux-async/src/containers/Counter/Counter.js
@@ -4,7 +4,7 @@ import {connect} from 'react-redux';
 import CounterControl from '../../components/CounterControl/CounterControl';
 import CounterOutput from '../../components/CounterOutput/CounterOutput';
 
-import * as actionTypes from '../../store/actions';
+import * as actionsCreators from '../../store/actions/actions';
 
 class Counter extends Component {
     state = {
@@ -18,8 +18,8 @@ class Counter extends Component {
                 <CounterOutput value={this.props.ctr} />{/*com redux*/}
                 <CounterControl label="Increment" clicked={this.props.onIncrementCounter} />
                 <CounterControl label="Decrement" clicked={this.props.onDecrementCounter}  />
-                <CounterControl label="Add 5" clicked={this.props.onAddQtdToCounter}  />
-                <CounterControl label="Subtract 5" clicked={this.props.onSubtractQtdToCounter}  />
+                <CounterControl label="Add 5" clicked={()=>{this.props.onAddQtdToCounter(5)}}  />
+                <CounterControl label="Subtract 5" clicked={()=>{this.props.onSubtractQtdToCounter(5)}}  />
                 <hr />
                 <button onClick={()=>{this.props.onStoreResult(this.props.ctr)}}>Store Result</button>
                 <ul>
@@ -41,12 +41,12 @@ const mapStateToProps = state =>{//nomenclatura é para transformar o stado da s
 
 const mapDispatchToProps = dispatch =>{ //passa uma action via props para o componente
     return { 
-        onIncrementCounter: () => dispatch({type: actionTypes.INCREMENT}),
-        onDecrementCounter: () => dispatch({type: actionTypes.DECREMENT}),
-        onAddQtdToCounter: () => dispatch({type: actionTypes.ADD_QTD, amount:5}),
-        onSubtractQtdToCounter: () => dispatch({type: actionTypes.SUBTRACT_QTD, amount:5}),
-        onStoreResult: (res)=> dispatch({type: actionTypes.STORE_RESULT, result:res}),
-        onDeleteResult: (id)=> dispatch({type: actionTypes.DELETE_RESULT, id:id})
+        onIncrementCounter: () => dispatch(actionsCreators.increment()),
+        onDecrementCounter: () => dispatch(actionsCreators.decrement()),
+        onAddQtdToCounter: (qtd) => dispatch(actionsCreators.addQtd(qtd)),
+        onSubtractQtdToCounter: (qtd) => dispatch(actionsCreators.subtractQtd(qtd)),
+        onStoreResult: (res) => dispatch(actionsCreators.asyncStoreResult(res)),
+        onDeleteResult: (id) => dispatch(actionsCreators.deleteResult(id))
     }
 };
 
